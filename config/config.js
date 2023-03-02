4f348371affda0629a7fcd6e68ba2526d583a4fa
Kanishk Bisen <bisenkanishk16@gmail.com>
2023-03-02 12:58:59 +0530

    mongo db link change

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/weshare-pro")
+// mongoose.connect("mongodb://localhost:27017/weshare-pro")
 
-// mongoose.connect("mongodb+srv://Gangareddy:Reddy12@cluster0.hjhorxm.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Gangareddy:Reddy12@cluster0.hjhorxm.mongodb.net/?retryWrites=true&w=majority")
